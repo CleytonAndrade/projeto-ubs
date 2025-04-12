1601025c1b98b86266bb27f2f253f2ba2168e69e
CleytonAndrade <clyadr@gmail.com>
2025-04-11 22:35:19 -0300

    Limpando as css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const itens = document.querySelector(".menu")
+const itens = document.querySelector(".menu");
+
 function clickMenu() {
-    itens.style.display === 'none'? itens.style.display = 'block':itens.style.display = 'none'
+    itens.classList.toggle("ativo");
 }
