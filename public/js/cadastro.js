66f5e66b50607c5920b77cdbb8f34fe34fd58d54
CleytonAndrade <clyadr@gmail.com>
2025-04-15 16:12:52 -0300

    fix: correct selector for username input in cadastro.js

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -4,7 +4,7 @@
         e.preventDefault();
         const dados = {
             nome: document.querySelector("#name").value,
-            usuario: document.querySelector("#user").value,
+            usuario: document.querySelector("#username").value,
             senha: document.querySelector("#password").value,
             email: document.querySelector("#email").value,
             telefone: document.querySelector("#tel").value,
