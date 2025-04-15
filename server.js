29b3071231505ee8450778f3e3b5df9143ac2712
CleytonAndrade <clyadr@gmail.com>
2025-04-15 17:16:17 -0300

    fix: update server log message for consistency

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.post('/cadastro', (req, res) => {
 
 // Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
