7dd7094c726849254d379c98477307a88cdb04df
CleytonAndrade <clyadr@gmail.com>
2025-04-15 10:54:52 -0300

    adiciona atributos de autocomplete aos campos de usuário e senha no formulário de login

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ const button = document.querySelector("#entrar");
 button.addEventListener("click", (e) => {
     e.preventDefault();
     const ojectLogin = getObjectLogin(user.value, pass.value);
-    console.log(ojectLogin)
+    const isValid = validateUser(ojectLogin.usuario, ojectLogin.senha);
 });	
 
 function getObjectLogin(user, password) {
@@ -15,3 +15,18 @@ function getObjectLogin(user, password) {
     }
 }
 
+function validateUser(user, password) {
+    if(!user || !password) {
+        alert("Preencha todos os campos!");
+        return false;
+    }
+    if(user.length < 3) {
+        alert("O nome de usuário deve ter pelo menos 3 caracteres!");
+        return false;
+    }
+    if(password.length < 6) {
+        alert("A senha deve ter pelo menos 6 caracteres!");
+        return false;
+    }
+    return true;    
+}
