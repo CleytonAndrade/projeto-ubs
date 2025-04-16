181c103cf953fd688a666bae7ef4d9df18e18aa7
CleytonAndrade <clyadr@gmail.com>
2025-04-16 17:16:20 -0300

    feat: update menu interaction by removing inline click handler and adding event listener in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 (function(){
+  document.getElementById('menu-burguer').addEventListener('click', clickMenu);
   function clickMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('ativo');
