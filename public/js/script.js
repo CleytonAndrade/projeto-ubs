64156f95426f9cd97e2e3c5c6f25ebd60bcd6116
CleytonAndrade <clyadr@gmail.com>
2025-04-16 17:24:52 -0300

    refactor: replace getElementById with querySelector for menu interaction in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 (function(){
-  document.getElementById('menu-burguer').addEventListener('click', clickMenu);
+  document.querySelector('#menu-burguer').addEventListener('click', clickMenu);
   function clickMenu() {
-    const menu = document.getElementById('menu');
+    const menu = document.querySelector('#menu');
     menu.classList.toggle('ativo');
   }
 })();
