0a08071739a84f2b7c455d742f9d4122cb04d35f
CleytonAndrade <clyadr@gmail.com>
2025-04-18 14:49:53 -0300

    feat: add logout button visibility toggle and improve welcome message handling

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,44 @@
-(function(){
-  document.querySelector('#menu-burguer').addEventListener('click', clickMenu);
-  function clickMenu() {
-    const menu = document.querySelector('#menu');
-    menu.classList.toggle('ativo');
-  }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const nome = localStorage.getItem('nome');  // Recupera o nome armazenado
-    if (nome) {
-        // Atualiza o texto de boas-vindas
-        document.getElementById('welcome-message').innerText = `Bem-vindo, ${nome}!`;
+(function () {
+    document
+        .querySelector("#menu-burguer")
+        .addEventListener("click", clickMenu);
+    function clickMenu() {
+        const menu = document.querySelector("#menu");
+        menu.classList.toggle("ativo");
     }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const nome = localStorage.getItem("nome"); // Recupera o nome armazenado
+        if (nome) {
+            // Atualiza o texto de boas-vindas
+            document.getElementById(
+                "welcome-message"
+            ).innerText = `Bem-vindo, ${nome}!`;
+        }
     });
 
-    document.querySelector('#logout').addEventListener('click', () => {
-        localStorage.removeItem("nome")
-        window.location.href = "/"
+    document.querySelector("#logout").addEventListener("click", () => {
+        localStorage.removeItem("nome");
+        window.location.href = "/";
     });
 
     document.addEventListener("DOMContentLoaded", () => {
-      const nome = localStorage.getItem("nome");
-      const welcome = document.getElementById("welcome-message");
-      const botaoLogout = document.getElementById("logout");
-  
-      if (nome) {
-          if (welcome) welcome.textContent = `Bem-vindo, ${nome}!`;
-          if (botaoLogout) botaoLogout.classList.add("mostrar");
-      }
-  
-      if (botaoLogout) {
-          botaoLogout.addEventListener("click", () => {
-              localStorage.removeItem("nome");
-              window.location.href = "/login"; // redireciona após logout
-          });
-      }
-  });
-  
+        const nome = localStorage.getItem("nome");
+        const welcome = document.getElementById("welcome-message");
+        const botaoLogout = document.getElementById("logout");
+
+        if (nome) {
+            if (welcome) welcome.textContent = `Bem-vindo, ${nome}!`;
+            if (botaoLogout) botaoLogout.classList.add("mostrar");
+        } else {
+            if (botaoLogout) botaoLogout.classList.remove("mostrar");
+        }
+
+        if (botaoLogout) {
+            botaoLogout.addEventListener("click", () => {
+                localStorage.removeItem("nome");
+                window.location.href = "/login";
+            });
+        }
+    });
 })();
